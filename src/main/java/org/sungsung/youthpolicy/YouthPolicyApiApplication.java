0f52e728c81b59f2a91d82c69ac14ead389d3f9b
김종성 <2002dreams@daum.net>
2026-02-09 14:00:08 +0900

    init spring project

---
+++ src/main/java/org/sungsung/youthpolicy/YouthPolicyApiApplication.java
@@ -0,0 +1,13 @@
+package org.sungsung.youthpolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthPolicyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthPolicyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sungsung/youthpolicy/YouthPolicyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sungsung.youthpolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthPolicyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
