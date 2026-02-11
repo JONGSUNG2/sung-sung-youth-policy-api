c3c116b35d688e4dfde8ff53d780ac37bf18947b
ChoiSungWon <spancho0810@naver.com>
2026-02-11 10:22:55 +0900

    feat: 홈페이지 header 로그인,회원가입,로그아웃 버튼 추가 및 로그인 연동 추가

--- src/main/java/org/sungsung/youthpolicy/service/member/MemberService.java
+++ src/main/java/org/sungsung/youthpolicy/service/member/MemberService.java
@@ -4,7 +4,11 @@ import jakarta.servlet.http.HttpSession;
 import org.sungsung.youthpolicy.domain.dto.member.LoginDTO;
 import org.sungsung.youthpolicy.domain.vo.member.MemberVO;
 
+import java.util.Optional;
+
+
 public interface MemberService {
     void insert(MemberVO memberVO);
     boolean loginCheck(LoginDTO loginDTO, HttpSession session);
+    Optional<MemberVO> findMemberById(Long id);
 }
